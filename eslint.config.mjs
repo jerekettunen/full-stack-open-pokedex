d7c7aab6f87554b6ecc0fa7e24907c376d57ce12
jerekettunen <jereatkettunen@gmail.com>
2025-05-01 17:44:51 +0300

    10.6 test v3

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,35 +1,18 @@
 import globals from 'globals'
 import js from '@eslint/js'
 import stylisticJs from '@stylistic/eslint-plugin-js'
-import reactPlugin from 'eslint-plugin-react'
-import jestPlugin from 'eslint-plugin-jest'
 
 export default [
-  {
-    ignores: ['dist/**', 'build/**', 'coverage/**'],
-  },
   js.configs.recommended,
-  reactPlugin.configs.recommended,
   {
-    files: ['**/*.js', '**/*.jsx'],
+    files: ['**/*.js'],
     languageOptions: {
-      sourceType: 'module',
-      globals: {
-        ...globals.node,
-        ...globals.browser,
-        ...jestPlugin.environments.globals,
-      },
+      sourceType: 'commonjs',
+      globals: { ...globals.node },
       ecmaVersion: 'latest',
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true,
-        },
-      },
     },
     plugins: {
       '@stylistic/js': stylisticJs,
-      react: reactPlugin,
-      jest: jestPlugin,
     },
     rules: {
       '@stylistic/js/indent': ['error', 2],
@@ -37,17 +20,13 @@ export default [
       '@stylistic/js/quotes': ['error', 'single'],
       '@stylistic/js/semi': ['error', 'never'],
       eqeqeq: 'error',
-      '@stylistic/js/no-trailing-spaces': 'error',
-      '@stylistic/js/object-curly-spacing': ['error', 'always'],
-      '@stylistic/js/arrow-spacing': ['error', { before: true, after: true }],
-      'no-console': 'error',
-      'react/prop-types': 0,
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': ['error', 'always'],
+      'arrow-spacing': ['error', { before: true, after: true }],
+      'no-console': 'off',
     },
   },
   {
-    files: ['app.js', 'server/**/*.js'],
-    languageOptions: {
-      sourceType: 'commonjs',
-    },
+    ignores: ['dist/**'],
   },
 ]
